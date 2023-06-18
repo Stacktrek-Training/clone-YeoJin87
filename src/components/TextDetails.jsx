ed435fbcc92706d2f8e66d31bb90c844ad724a8c
johanblupojr <johanblupojr@gmail.com>
2023-06-18 21:18:25 +0800

    Commit TextDetails jsx

---
+++ src/components/TextDetails.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TextDetails() {
+  return (
+    <>
+      <h1>Spring Preview Sale</h1>
+      <br />
+      <p>Rugs, vases and other objects covered in fur are a rarity</p>
+      <p>The only exception is at the entrance to the room</p>
+      <p>where you enter with your sword drawn</p>
+    </>
+  );
+}
+
+export default TextDetails;
