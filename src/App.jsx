53abe70a6cd9c63da520a12b7eae8e02853e31a2
YeoJin <eugeneflores87@gmail.com>
2023-06-18 20:35:30 +0800

    6-18 changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
 import React from 'react'
 //import './App.css'
-import Navbar from "./Navbar"
+//import Navbar from "./Navbar"
 import "./styles.css"
 
+const logo =  'https://drive.google.com/file/d/1wEb597clpIEt3z4_laIxdn3RZRtORWLM/view?usp=sharing'
+function NavNames() {
+    const data = [
+        {
+            'nav': 'Home',
+            'link': '#home'
+        },
+        {
+            'nav': 'About',
+            'link': '#about'
+        },
+        {
+            'nav': 'Services',
+            'link': '#services'
+        },
+        {
+            'nav': 'Contact',
+            'link': '#contact'
+        }
+    ]
+    const listItems = data.map((element) => {
+        return (
+            <ul style = {{listStyle: 'none'}}>
+                <li><a href={element.link}>{element.nav}</a></li>
+            </ul>
+        )
+    }
+    )
+    return (
+        <div className='navigation'>
+            {listItems}
+        </div>
+    )
+}
 function App() {
- return(
-  <Navbar />
-
+ return (
+    <div className='App'>
+        <div className='nav-bar'>
+            
+           <h2>Salford & Co.</h2>
+            <NavNames />
+        </div>
+    </div>
  )
- 
 }
 
 export default App
